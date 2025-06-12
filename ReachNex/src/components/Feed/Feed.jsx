5e85819101127e7397ee80bfc921aa4f86ad6b02
ranaasad07 <ranaasad1407@gmail.com>
2025-06-12 23:43:36 +0500

    feed page

---
+++ ReachNex/src/components/Feed/Feed.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+const Feed = () => {
+  return (
+    <div>
+
+        <Navbar></Navbar>
+
+    </div>
+
+  )
+}
+
+export default Feed
