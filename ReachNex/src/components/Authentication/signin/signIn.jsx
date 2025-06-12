878e901622f873607647939ad6ddef54aec12001
sajawalDev <sajawal.21aug24webbpt@gmail.com>
2025-06-12 22:38:14 +0500

    username added

--- ReachNex/src/components/Authentication/signin/signIn.jsx
+++ ReachNex/src/components/Authentication/signin/signIn.jsx
@@ -25,7 +25,7 @@ const SignInForm = () => {
             const decoded = jwtDecode(token);
             const { username, fullname, email, id } = decoded;
             // setUser({ username, fullname, email, id });
-            navigate('/landing');
+            navigate(`/feed/${username}`);
         } catch (err) {
             alert('Invalid credentials');
         }
