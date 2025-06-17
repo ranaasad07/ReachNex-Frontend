cc5994111f76dfd36a2225fd211fd6ebd2c5c49c
Aman - umer <amanumer676@gmail.com>
2025-06-17 22:00:32 +0500

    add notification

--- ReachNex/src/Pages/Notifications/LinkedInCard.jsx
+++ ReachNex/src/Pages/Notifications/LinkedInCard.jsx
@@ -1,12 +1,11 @@
 // src/components/LinkedInCard.jsx
 import React from 'react';
 import styles from './LinkedInCard.module.css';
-// import profilePic from '../assets/profile.png'; // Aap koi bhi placeholder image use kar sakte ho
+
 
 const LinkedInCard = ({ message }) => {
   return (
     <div className={styles.card}>
-      {/* <img src={profilePic} alt="Profile" className={styles.avatar} /> */}
       <div className={styles.content}>
         <div className={styles.name}>Inshrah Tariq</div>
         <div className={styles.message}>{message}</div>
