5ee8ef91960d2ec0316d3f9a6ee6f0cbb91cdf68
Faizan-code104 <fiazan.19aug24webbpt@gmail.com>
2025-07-10 07:26:29 -0700

    notification

--- ReachNex/src/Pages/Notifications/LinkedInCard.jsx
+++ ReachNex/src/Pages/Notifications/LinkedInCard.jsx
@@ -7,7 +7,7 @@ const LinkedInCard = ({ message }) => {
   return (
     <div className={styles.card}>
       <div className={styles.content}>
-        <div className={styles.name}>Inshrah Tariq</div>
+        <div className={styles.name}>Faizan Tariq</div>
         <div className={styles.message}>{message}</div>
         <div className={styles.time}>1h</div>
       </div>
